b5b4588a315eac919d77a3f788171440843a2753
rudkevych <oksana.rudkevych@gmail.com>
2022-09-06 22:20:28 +0200

    feat: added error styling for incorrect input

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,30 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
+    console.log(event.target.value);
+    if(event.target.value.trim().length) {
+      setIsValid(true);
+    }
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(!enteredValue.trim().length) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{color: isValid ? 'black' : 'red'}}>Course Goal</label>
+        <input style={{borderColor: isValid ? 'black' : 'red'}}type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
